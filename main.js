8418e7bc40529bf22dcfd4c731139e2fd7c7fe65
Christopher Kistner <chris.kistner0@gmail.com>
2018-10-08 10:22:55 -0700

    completed basic puzzle generation sequence for Easy Mode

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", function (){
     //Generate Play Field
     const playField = document.querySelector(".playField");
-    console.log(playField);
+    const easyMode = document.querySelector(".lv1")
     function createBoard () {
         return `
         <div class = "gameButton">
@@ -18,12 +18,58 @@ const renderBoard = createGrid(15);
 playField.innerHTML = renderBoard.join('\n');
 
 const playButtons = document.querySelectorAll(".gameButton");
+// Generates the random layout of the start of  the puzzle.
+function generatePuzzle (difficulty) {
+    const answer = [];
 
+    while(answer.length <= difficulty){
+        const random = Math.floor(Math.random()* 16)
+        if(answer.indexOf(random) === -1){
+            answer.push(random)    
+        } 
+    }
+    
+    return answer;
+}
+// Play Easy Mode
+function playEasy () {
+    let answer = generatePuzzle(4);
+    const inputs = Array.from(playButtons);
+
+
+    delay(answer.length - 1, 1500, showPuzzle(answer, inputs), clearBoard)
+
+}
+
+function clearBoard(){
+    const inputs = Array.from(playButtons);
+    console.log('clearing')
+    inputs.forEach(ele => ele.classList.remove('highlighted'))
+}
+
+function showPuzzle (answer,inputs) {
+    let index = 0
+    return function(){
+        inputs[answer[index]].classList.add('highlighted')
+        index++
+    }
+}
+
+function delay(numberOfTimes, milliseconds, cb, lastAction){
+    if(numberOfTimes < 0) return lastAction()
+    setTimeout(function(){
+        cb()
+        delay(numberOfTimes -1, milliseconds, cb, lastAction)
+    },milliseconds)
+}
+
+
+//Event Listeners
 playButtons.forEach(cell => cell.addEventListener("mouseover", function(){
     cell.classList.add('highlighted');
 }));
-
 playButtons.forEach(cell => cell.addEventListener("mouseout", function() {
     cell.classList.remove('highlighted')
 }));
+easyMode.addEventListener('click', playEasy);
 })
