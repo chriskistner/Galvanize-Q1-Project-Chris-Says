a1db38e68b75d3c117d9d7a9c2838dd7ba1c7965
Christopher Kistner <chris.kistner0@gmail.com>
2018-10-08 12:08:56 -0700

    fixed timing functionality on Loop and got basic answerchecker working

--- main.js
+++ main.js
@@ -2,9 +2,10 @@ document.addEventListener("DOMContentLoaded", function (){
     //Generate Play Field
     const playField = document.querySelector(".playField");
     const easyMode = document.querySelector(".lv1")
-    function createBoard () {
+    const submit = document.querySelector(".Submit");
+    function createBoard (ID) {
         return `
-        <div class = "gameButton">
+        <div class = "gameButton" data-id="${ID}">
         </div>`
     }
     function createGrid (num) {
@@ -19,31 +20,28 @@ playField.innerHTML = renderBoard.join('\n');
 
 const playButtons = document.querySelectorAll(".gameButton");
 // Generates the random layout of the start of  the puzzle.
+let answer = [];
 function generatePuzzle (difficulty) {
-    const answer = [];
-
-    while(answer.length <= difficulty){
+    while(answer.length < difficulty){
         const random = Math.floor(Math.random()* 16)
         if(answer.indexOf(random) === -1){
             answer.push(random)    
         } 
     }
-    
+    console.log(answer); 
     return answer;
+
 }
+
 // Play Easy Mode
 function playEasy () {
     let answer = generatePuzzle(4);
-    const inputs = Array.from(playButtons);
-
-
-    delay(answer.length - 1, 1500, showPuzzle(answer, inputs), clearBoard)
-
+    let inputs = Array.from(playButtons);
+    delay(answer.length -1, 1500, showPuzzle(answer, inputs), clearBoard)
 }
 
 function clearBoard(){
     const inputs = Array.from(playButtons);
-    console.log('clearing')
     inputs.forEach(ele => ele.classList.remove('highlighted'))
 }
 
@@ -54,14 +52,45 @@ function showPuzzle (answer,inputs) {
         index++
     }
 }
-
 function delay(numberOfTimes, milliseconds, cb, lastAction){
-    if(numberOfTimes < 0) return lastAction()
+
+    if(numberOfTimes < 0) return setTimeout(lastAction,milliseconds)
     setTimeout(function(){
         cb()
+        console.log(numberOfTimes);
         delay(numberOfTimes -1, milliseconds, cb, lastAction)
     },milliseconds)
 }
+function checkAnswer() {
+    for (keys of submission) {
+
+    } 
+}
+
+let submission = [];
+function inputAnswer(event) {
+    submission.push(event.target.getAttribute('data-id'));
+    console.log(submission);
+}
+
+function checkAnswer() {
+    for (let i = 0; i < answer.length; i++) {
+        let ans = answer[i];
+        for (let x = 0; x < submission.length; x++) {
+            let subM = submission[x];
+            if (subM != ans){
+                submit.textContent = `Incorrect`
+            } else {
+                submit.textContent= 'Correct';
+            }
+        }
+    }
+    if (answer.length !== submission.length) {
+        submit.textContent = `Incorrect`
+    }
+}
+
+
 
 
 //Event Listeners
@@ -71,5 +100,7 @@ playButtons.forEach(cell => cell.addEventListener("mouseover", function(){
 playButtons.forEach(cell => cell.addEventListener("mouseout", function() {
     cell.classList.remove('highlighted')
 }));
-easyMode.addEventListener('click', playEasy);
+playButtons.forEach(cell => cell.addEventListener("click", inputAnswer));
+easyMode.addEventListener('click', playEasy)
+submit.addEventListener('click', checkAnswer);
 })
