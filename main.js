d6aa230a351e08278db576766600ca2e5dbba9e3
Christopher Kistner <chris.kistner0@gmail.com>
2018-10-08 15:16:26 -0700

    Added Scoring functionality

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ document.addEventListener("DOMContentLoaded", function (){
     //Generate Play Field
     const playField = document.querySelector(".playField");
     const easyMode = document.querySelector(".lv1")
+    const normalMode = document.querySelector(".lv2")
+    const hardMode= document.querySelector(".lv3")
     const submit = document.querySelector(".Submit");
     function createBoard (ID) {
         return `
@@ -28,17 +30,36 @@ function generatePuzzle (difficulty) {
             answer.push(random)    
         } 
     }
-    console.log(answer); 
     return answer;
 
 }
+//Play Modes
+let startingDifficulty;
 
 // Play Easy Mode
 function playEasy () {
     let answer = generatePuzzle(4);
+    startingDifficulty = 4;
     let inputs = Array.from(playButtons);
     delay(answer.length -1, 1500, showPuzzle(answer, inputs), clearBoard)
 }
+// Start a Game on Medium Difficulty
+function playMedium () {
+    let answer = generatePuzzle(5);
+    console.log(answer);
+    startingDifficulty = 5;
+    let inputs = Array.from(playButtons);
+    delay(answer.length -1, 1200, showPuzzle(answer, inputs), clearBoard)
+}
+//Start a Game on Hard Difficulty
+function playHard () {
+    let answer = generatePuzzle(6);
+    console.log(answer);
+    startingDifficulty = 6;
+    let inputs = Array.from(playButtons);
+    delay(answer.length -1, 800, showPuzzle(answer, inputs), clearBoard)
+}
+
 
 function clearBoard(){
     const inputs = Array.from(playButtons);
@@ -57,50 +78,71 @@ function delay(numberOfTimes, milliseconds, cb, lastAction){
     if(numberOfTimes < 0) return setTimeout(lastAction,milliseconds)
     setTimeout(function(){
         cb()
-        console.log(numberOfTimes);
         delay(numberOfTimes -1, milliseconds, cb, lastAction)
     },milliseconds)
 }
-function checkAnswer() {
-    for (keys of submission) {
 
-    } 
-}
-
+// Answer Check;
 let submission = [];
 function inputAnswer(event) {
     submission.push(event.target.getAttribute('data-id'));
     console.log(submission);
+    event.target.classList.add("selected");
 }
 
 function checkAnswer() {
-    for (let i = 0; i < answer.length; i++) {
-        let ans = answer[i];
-        for (let x = 0; x < submission.length; x++) {
-            let subM = submission[x];
-            if (subM != ans){
-                submit.textContent = `Incorrect`
-            } else {
-                submit.textContent= 'Correct';
-            }
-        }
-    }
     if (answer.length !== submission.length) {
         submit.textContent = `Incorrect`
     }
+    if (arrayCheck(answer, submission)) {
+        submit.textContent=`Correct`;
+        updateScores(startingDifficulty);
+    } else {submit.textContent = `Incorrect`}
 }
 
+function arrayCheck (arr1, arr2) {
+    return arr1.toString() === arr2.toString();
+}
 
+//Score Calculator
+let hiScore = document.querySelector('.hScore');
+let currentScore = document.querySelector('.cScore');
 
-
+function updateScores (difficulty) {
+    let hiScoreVal = Number(hiScore.textContent);
+    let scoreVal = Number(currentScore.textContent);
+    console.log(hiScoreVal);
+    if (difficulty === 4) {
+        scoreVal += scoreVal + (answer.length * difficulty)
+        currentScore.textContent= `${scoreVal}`;
+        if (scoreVal > hiScoreVal) {
+            hiScore.textContent = `${scoreVal}`;
+        }
+    }
+    else if (difficulty === 5) {
+        scoreVal += scoreVal + (answer.length * difficulty)
+        currentScore.textContent= `${scoreVal}`;
+        if (scoreVal > hiScoreVal) {
+            hiScore.textContent = `${scoreVal}`;
+        }
+    } else {
+        scoreVal += scoreVal + (answer.length *difficulty)
+        currentScore.textContent =`${scoreVal}`;
+        if (scoreVal > hiScoreVal) {
+            hiScore.textContent = `${scoreVal}`;
+        }
+    }
+}
 //Event Listeners
 playButtons.forEach(cell => cell.addEventListener("mouseover", function(){
-    cell.classList.add('highlighted');
+    cell.classList.add('hover');
 }));
 playButtons.forEach(cell => cell.addEventListener("mouseout", function() {
-    cell.classList.remove('highlighted')
+    cell.classList.remove('hover')
 }));
 playButtons.forEach(cell => cell.addEventListener("click", inputAnswer));
-easyMode.addEventListener('click', playEasy)
+easyMode.addEventListener('click', playEasy);
+normalMode.addEventListener('click', playMedium);
+hardMode.addEventListener('click', playHard);
 submit.addEventListener('click', checkAnswer);
 })
